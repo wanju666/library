13082a74a3def9906c18566160e4c06f05de000d
wanju666 <2354504303@qq.com>
2019-01-09 04:31:49 +0800

    '实现登录获取openid'

--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -1,12 +1,46 @@
 <template>
     <div>
         图书页面
+        <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="doLogin">获取用户信息</button>
     </div>
 </template>
 
 <script>
-export default {
+import qcloud from 'wafer2-client-sdk'
+import config from '../../config'
 
+export default {
+    methods:{
+        doLogin: function () {
+            const session = qcloud.Session.get()
+
+            if (session) {
+                // 第二次登录
+                // 或者本地已经有登录状态
+                // 可使用本函数更新登录状态
+                qcloud.loginWithCode({
+                    loginUrl: config.loginUrl,
+                    success: function (userInfo) {
+                        console.log('已经登录成功', userInfo);
+                    },
+                    fail: function (err) {
+                        console.log('登录失败', err);
+                    }
+                })
+            } else {
+                // 首次登录
+                qcloud.setLoginUrl(config.loginUrl);
+                qcloud.login({
+                    success: function (userInfo) {
+                        console.log('登录成功', userInfo);
+                    },
+                    fail: function (err) {
+                        console.log('登录失败', err);
+                    }
+                });
+            }
+        }   
+    }
 }
 </script>
 
